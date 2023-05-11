cf0abcc86fa6440a73cc5b1db3daf75bb385cff9
brandongrier7 <bgrier@udel.edu>
2023-05-11 18:42:10 -0400

    attempt

--- compute.c
+++ compute.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <math.h>
+#include <openacc.h>
 #include "vector.h"
 #include "config.h"
 
@@ -12,16 +13,20 @@ void compute(){
 	int i,j,k;
 	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
 	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+	#pragma acc parallel loop
 	for (i=0;i<NUMENTITIES;i++)
 		accels[i]=&values[i*NUMENTITIES];
 	//first compute the pairwise accelerations.  Effect is on the first argument.
+	#pragma acc parallel loop
 	for (i=0;i<NUMENTITIES;i++){
+		#pragma acc parallel loop
 		for (j=0;j<NUMENTITIES;j++){
 			if (i==j) {
 				FILL_VECTOR(accels[i][j],0,0,0);
 			}
 			else{
 				vector3 distance;
+				#pragma acc parallel loop
 				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
 				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
 				double magnitude=sqrt(magnitude_sq);
@@ -31,14 +36,18 @@ void compute(){
 		}
 	}
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	#pragma acc parallel loop
 	for (i=0;i<NUMENTITIES;i++){
 		vector3 accel_sum={0,0,0};
+		#pragma acc parallel loop
 		for (j=0;j<NUMENTITIES;j++){
+			#pragma acc parallel loop reductions(accel_sum:accel_sums[k])
 			for (k=0;k<3;k++)
 				accel_sum[k]+=accels[i][j][k];
 		}
 		//compute the new velocity based on the acceleration and time interval
 		//compute the new position based on the velocity and time interval
+		#pragma acc parallel loop
 		for (k=0;k<3;k++){
 			hVel[i][k]+=accel_sum[k]*INTERVAL;
 			hPos[i][k]=hVel[i][k]*INTERVAL;
